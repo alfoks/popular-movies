85d8fa8728dd1a5b39fac22da21a84d939c6909c
Alex Fokas <alfoks@yahoo.com>
2018-02-19 12:53:10 +0200

    Added an interface to be used by Retrofit for the TheMovieDB API.
    Signed-off-by: Alex Fokas <alfoks@yahoo.com>

---
+++ app/src/main/java/gr/alfoks/popularmovies/utils/TheMovieDbApi.java
@@ -0,0 +1,29 @@
+package gr.alfoks.popularmovies.utils;
+
+import gr.alfoks.popularmovies.model.Movies;
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface TheMovieDbApi {
+    enum SortBy {
+        POPULAR("popular"),
+        TOP_RATED("top_rated");
+
+        private final String value;
+
+        SortBy(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    String BASE_URL = "https://api.themoviedb.org/";
+
+    @GET("/3/movie/{sort-by}")
+    Single<Movies> getMovies(@Path("sort-by") SortBy sortBy);
+}
