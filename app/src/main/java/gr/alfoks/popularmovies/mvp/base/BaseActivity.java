763d805cb2a843424b842e08e536a06475a5d0b1
Alex Fokas <alfoks@yahoo.com>
2018-03-02 15:55:38 +0200

    Don't recreate MainActivity when returning from child Activities. Added connection state detection in base activity.

--- app/src/main/java/gr/alfoks/popularmovies/mvp/base/BaseActivity.java
+++ app/src/main/java/gr/alfoks/popularmovies/mvp/base/BaseActivity.java
@@ -1,17 +1,30 @@
 package gr.alfoks.popularmovies.mvp.base;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import gr.alfoks.popularmovies.R;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>>
     extends AppCompatActivity implements
     MvpView {
 
     private P presenter;
+
+    @BindView(R.id.txtNoConnection)
+    TextView txtConnection;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,13 +35,25 @@ public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>>
         ButterKnife.bind(this);
 
         presenter.attach(getThis());
+        registerConnectivityReceiver();
         init(savedInstanceState);
     }
 
     @Override
     protected void onDestroy() {
+        unregisterConnectivityReceiver();
         presenter.detach();
         super.onDestroy();
+    }
+
+    private void registerConnectivityReceiver() {
+        registerReceiver(
+            connectivityChangeReceiver,
+            new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    private void unregisterConnectivityReceiver() {
+        unregisterReceiver(connectivityChangeReceiver);
     }
 
     /**
@@ -42,5 +67,39 @@ public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>>
 
     protected P getPresenter() {
         return presenter;
+    }
+
+    private final BroadcastReceiver connectivityChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int connectionType = getConnectionType(context);
+            boolean connectionOn = connectionType == ConnectivityManager.TYPE_WIFI
+                || connectionType == ConnectivityManager.TYPE_MOBILE;
+
+            onConnectivityChangedPrivate(connectionOn);
+        }
+    };
+
+    private int getConnectionType(Context context) {
+        NetworkInfo activeNetwork = null;
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(cm != null) {
+            activeNetwork = cm.getActiveNetworkInfo();
+        }
+        if(activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+            return activeNetwork.getType();
+        }
+        return -1;
+    }
+
+    private void onConnectivityChangedPrivate(boolean connectionOn) {
+        if(txtConnection != null) {
+            txtConnection.setVisibility(connectionOn ? View.GONE : View.VISIBLE);
+        }
+        onConnectivityChanged(connectionOn);
+    }
+
+    protected void onConnectivityChanged(boolean connectionOn) {
+
     }
 }
